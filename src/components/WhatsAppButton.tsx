47551dd924ca09c1faae3bc37224fb11ce8ea764
bobsa143 <boubleh@gmail.com>
2026-03-06 21:56:56 +0000

    Updated WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -12,7 +12,7 @@ export default function WhatsAppButton() {
       aria-label="Contact on WhatsApp"
     >
       <img
-        src="https://blog.logomyway.com/wp-content/uploads/2017/02/whatsapp-icon.jpg"
+        src="https://png.pngtree.com/element_our/sm/20180626/sm_5b321c99945a2.png"
         alt="WhatsApp"
         className="h-6 w-6"
       />
